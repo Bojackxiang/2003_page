b61113550cdf32719fa52d896416e14a4f236d02
weijiexiang <weijiexiang@weijiexiangdeMacBook-Pro.local>
2018-07-05 19:21:34 +1000

    style update

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
+    // *****************************************************
+    // button sliding 
     $("#sliding").click(function(){
         $('html, body').animate({
             scrollTop: $("#registerForm").offset().top
         }, 1000)
     });
+
+    $("#aisf-button").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#aisf-intro").offset().top
+        }, 1000)
+    });
+
+    $("#all-pass-button").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#all-pass-intro").offset().top
+        }, 1000)
+    });
+
+    $("#business-button").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#business-intro").offset().top
+        }, 1000)
+    });
+
+    $("#register-button").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#registerForm").offset().top
+        }, 1000)
+    });
+
+    $("#obtain").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#registerForm").offset().top
+        }, 1000)
+    });
+
+
 });
