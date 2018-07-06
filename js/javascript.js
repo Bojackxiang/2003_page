c5f32ef6e8b6fe306c28aa176125023aee454cb6
Bojackxiang <batmandocode@gmail.com>
2018-07-06 14:23:39 +1000

    u

--- js/javascript.js
+++ js/javascript.js
@@ -1,78 +1,92 @@
-$(document).ready(function () {
-    // *****************************************************
-    // button sliding 
-    $("#sliding").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#registerForm").offset().top
-        }, 1000)
+$(document).ready(function() {
+  // *****************************************************
+  // button sliding
+  $("#sliding").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#registerForm").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#aisf-button").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#aisf-intro").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#all-pass-button").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#all-pass-intro").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#business-button").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#business-intro").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#register-button").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#registerForm").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#obtain").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#registerForm").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#submit-button").click(function(e) {
+    alert("test");
+    e.preventDefault();
+    e.stopPropagation();
+
+    var name = $("#userName").val();
+    var school = $("#userSchool").val();
+    var emailAddress = $("#userEmail").val();
+    var major = $("#userMajor").val();
+
+    var body = {
+      name: name,
+      emailAddress: emailAddress,
+      school: school,
+      major: major
+    };
+
+    $.ajax({
+      url: "https://www.aisfexpo.com.au/api/submit",
+      type: "POST",
+      dataType: 'json',
+      data: body,
+      headers: {
+          'Conent-Type': 'application/x-www-form-urlencoded',
+      },
+
+      success: function(body, status, xhr) {
+        console.log(body);
+      },
+      error: function(body, status, xhr) {
+        console.log(body);
+      }
     });
-
-    $("#aisf-button").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#aisf-intro").offset().top
-        }, 1000)
-    });
-
-    $("#all-pass-button").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#all-pass-intro").offset().top
-        }, 1000)
-    });
-
-    $("#business-button").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#business-intro").offset().top
-        }, 1000)
-    });
-
-    $("#register-button").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#registerForm").offset().top
-        }, 1000)
-    });
-
-    $("#obtain").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#registerForm").offset().top
-        }, 1000)
-    });
-
-
-    $("#submit-button").click(
-        function (e) {
-            alert("test")
-            e.preventDefault();
-            e.stopPropagation();
-
-            var name = $("#userName").val();
-            var school = $("#userSchool").val();
-            var emailAddress = $("#userEmail").val();
-            var major = $("#userMajor").val();
-
-            var body = {
-                name: name,
-                emailAddress: emailAddress, 
-                school: school,
-                major: major,
-            }
-
-            $.ajax({
-                url: "https://www.aisfexpo.com.au/api/submit",
-                type: 'POST',
-                headers: {
-                    'Conent-Type': 'application/x-www-form-urlencoded',
-                },
-                
-                success: function (body, status, xhr) {
-                    console.log(body)
-                },
-                error: function (body, status, xhr) {
-                    console.log(body);
-                }
-            })
-        }
-    )
-
-
-
+  });
 });
