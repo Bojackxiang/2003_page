ae73903c4b41742f7c87a92633b39ab1baa5afab
Bojackxiang <batmandocode@gmail.com>
2018-07-18 14:50:47 +1000

    source1.3.5

--- js/tracking.js
+++ js/tracking.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     source = new URL(document.referrer).hostname;
   }
 
-  sourceBody = {"source":source, deUrl:window.location.search};
+  sourceBody = {"source":source, deUrl:window.decodeURIComponent(location.search)};
   console.log(sourceBody);
   // sending the information
   $.ajax({
