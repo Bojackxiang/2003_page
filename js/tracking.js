61e776809adc2bed98f3641a2603c06190f3987d
Bojackxiang <batmandocode@gmail.com>
2018-07-18 12:21:55 +1000

    console.log source

--- js/tracking.js
+++ js/tracking.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    console.log("writing file is ready")
-    var query_str = decodeURIComponent(window.location.search.substring(1));
-    console.log(window.location);
+    console.log("pv statistics")
+    console.log(document.referrer);
 });
