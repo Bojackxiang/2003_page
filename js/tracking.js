ae99798902c9ad0384d75a3789884c90bd71b39c
Bojackxiang <batmandocode@gmail.com>
2018-07-18 14:03:48 +1000

    source1.2.5

--- js/tracking.js
+++ js/tracking.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   console.log("pv statistics");
   // source url
   var source = document.referrer;
+  console.log(source);
 
   if (source.trim() == "") {
     source = "default";
