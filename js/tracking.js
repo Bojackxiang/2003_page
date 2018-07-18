60ffb90f2b0f58a5e318bae58320b4ed95c45676
Bojackxiang <batmandocode@gmail.com>
2018-07-18 11:46:33 +1000

    update

---
+++ js/tracking.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    console.log("writing file is ready")
+    var query_str = decodeURIComponent(window.location.search.substring(1));
+    console.log(window.location);
+});
