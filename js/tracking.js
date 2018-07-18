10c23190350343893d5b63d4e2125131d0593324
Bojackxiang <batmandocode@gmail.com>
2018-07-18 12:30:47 +1000

    source1.1

--- js/tracking.js
+++ js/tracking.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-    console.log("pv statistics")
-    var source = document.referrer;
-    if(source.trim() == ""){
-        console.log("get in from default");
-    }else{
-        var stringQuery = source.split("/");
-        console.log(stringQuery);
-    }
-    
+  console.log("pv statistics");
+  // source url
+  var source = document.referrer;
+  
+  if (source.trim() == "") {
+    source = "default";
+  } else {
+    var stringQuery = source.split("/");
+    source = stringQuery.reduce((first, second) => {
+      first.length > second.length ? first : second
+    });
+  }
 });
